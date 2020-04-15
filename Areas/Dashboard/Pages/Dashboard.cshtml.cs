f0bc2c7ec42fb0be19c18ac0b3842a94b257c572
Victor Moraes <victor.moraes@outlook.com.br>
2020-04-14 22:50:55 -0300

    Criação da página de dashboard

---
+++ Areas/Dashboard/Pages/Dashboard.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace AcquaJrApplication.Areas.Dashboard.Pages
+{
+    [Authorize]
+    public class DashboardModel : PageModel
+    {
+        private readonly ILogger<DashboardModel> _logger;
+
+        public DashboardModel(ILogger<DashboardModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
