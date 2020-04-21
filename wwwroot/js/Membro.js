15f9b9f1515f926ed811cc1618713235546ab1ae
Victor Moraes <victor.moraes@outlook.com.br>
2020-04-21 01:14:51 -0300

    Ajustes nas views do módulo de Membros

---
+++ wwwroot/js/Membro.js
@@ -0,0 +1,9 @@
+﻿function ValidarInputCpf(e) {
+    var tecla = (window.event) ? event.keyCode : e.which;
+
+    if ((tecla > 47 && tecla < 58)) return true;
+    else {
+        if (tecla == 8 || tecla == 0 || tecla == 44) return true;
+        else return false;
+    }
+}
