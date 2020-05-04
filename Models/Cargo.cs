666e30f67750cbe1f3c7e892caa7c99ed5d0d450
Victor Moraes <victor.moraes@outlook.com.br>
2020-05-03 23:08:17 -0300

    Ajustes de layout e interface gráfica

--- Models/Cargo.cs
+++ Models/Cargo.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AcquaJrApplication.Models
 {
@@ -32,7 +28,7 @@ namespace AcquaJrApplication.Models
         [Display(Name = "Honorário")]
         Honorario = 8,
 
-        [Display(Name = "Trainne")]
+        [Display(Name = "Trainee")]
         Trainne = 9
     }
 }
