a6f76e5e251f114c3d596bd2ec531166bef6dd0e
Victor Moraes <victor.moraes@outlook.com.br>
2020-05-23 21:43:32 -0300

    Ajustes de interface no módulo de Projetos

--- Areas/Dashboard/Pages/Projetos/Create.cshtml.cs
+++ Areas/Dashboard/Pages/Projetos/Create.cshtml.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using AcquaJrApplication.Data;
 using AcquaJrApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AcquaJrApplication.Areas.Dashboard.Pages.Projetos
 {
+    [Authorize]
     public class CreateModel : PageModel
     {
         private readonly AcquaJrApplication.Data.ApplicationDbContext _context;
--- Areas/Dashboard/Pages/Projetos/Edit.cshtml.cs
+++ Areas/Dashboard/Pages/Projetos/Edit.cshtml.cs
@@ -8,9 +8,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AcquaJrApplication.Data;
 using AcquaJrApplication.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AcquaJrApplication.Areas.Dashboard.Pages.Projetos
 {
+    [Authorize]
     public class EditModel : PageModel
     {
         private readonly AcquaJrApplication.Data.ApplicationDbContext _context;
