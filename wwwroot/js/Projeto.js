bd8e89047e65b5ae97008737d3a7f58f72fc078f
Victor Moraes <victor.moraes@outlook.com.br>
2020-05-27 21:52:50 -0300

    Ajustes e melhorias em funções javascript

--- wwwroot/js/Projeto.js
+++ wwwroot/js/Projeto.js
@@ -1,9 +1,1 @@
-﻿function ValidarInputNumerico(e) {
-    var tecla = (window.event) ? event.keyCode : e.which;
-
-    if ((tecla > 47 && tecla < 58)) return true;
-    else {
-        if (tecla == 8 || tecla == 0 || tecla == 44) return true;
-        else return false;
-    }
-}
+﻿
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,13 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your Javascript code.
-
-$(function () {
+﻿$(function () {
     $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(".input-numerico").keypress(function (e) {
+    var tecla = (window.event) ? event.keyCode : e.which;
+
+    if ((tecla > 47 && tecla < 58)) return true;
+    else {
+        if (tecla == 8 || tecla == 0 || tecla == 44) return true;
+        else return false;
+    }
 });
