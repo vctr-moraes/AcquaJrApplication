8ef82f1d30c62f05e7ceb7acff7f6d252dc0af75
Victor Moraes <victor.moraes@outlook.com.br>
2020-06-13 23:54:48 -0300

    Criação da página Sobre

---
+++ Pages/Sobre.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace AcquaJrApplication.Pages
+{
+    public class SobreModel : PageModel
+    {
+        private readonly ILogger<SobreModel> _logger;
+
+        public SobreModel(ILogger<SobreModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet() { }
+    }
+}
