0969ed3e9dbf72945f9746765a95b5224656b78c
Victor Alvarenga de Moraes <victor.moraes@outlook.com.br>
2020-09-05 21:27:49 -0300

    Personalização de botões e links.

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -16,7 +16,7 @@ using Microsoft.Extensions.Logging;
 
 namespace AcquaJrApplication.Areas.Identity.Pages.Account
 {
-    [Authorize]
+    //[Authorize]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
